102f142667392afb69d5968e3a4b1fd0c881b345
sirine hammami <hammamisyrinr28@gmail.com>
2023-04-23 21:50:48 +0100

    your commit message

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Entity;
+
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Utilisateur
+ *
+ * @ORM\Table(name="utilisateur")
+ * @ORM\Entity
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="utilisateur_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $utilisateurId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255, nullable=false)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=255, nullable=false)
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mdp", type="string", length=255, nullable=false)
+     */
+    private $mdp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role", type="string", length=255, nullable=false)
+     */
+    private $role;
+
+    public function getUtilisateurId(): ?int
+    {
+        return $this->utilisateurId;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+    public function getRoles(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRoles(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->adresse;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->adresse;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->mdp;
+    }
+
+    public function setPassword(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+
+}
+
+
+
+
---
+++ src/Form/UtilisateurType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+
+
+class UtilisateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nom', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Le champ nom ne doit pas être vide.',
+                ]),
+                new Length([
+                    'min' => 2,
+                    'max' => 50,
+                    'minMessage' => 'Le champ nom doit contenir au moins {{ limit }} caractéres.',
+                    'maxMessage' => 'Le champ nom doit contenir au maximum {{ limit }} caractéres.',
+                ]),
+            ]
+        ])
+        ->add('prenom', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Le champ prenom ne doit pas être vide.',
+                ]),
+                new Length([
+                    'min' => 2,
+                    'max' => 50,
+                    'minMessage' => 'Le champ prenom doit contenir au moins {{ limit }} caractéres.',
+                    'maxMessage' => 'Le champ prenom doit contenir au maximum {{ limit }} caractéres.',
+                ]),
+            ]
+        ])
+        ->add('adresse', EmailType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Le champ adresse email ne doit pas être vide.',
+                ]),
+                new Email([
+                    'message' => 'L\'adresse email "{{ value }}" n\'est pas valide.',
+                ]),
+            ]
+        ])
+        ->add('mdp', PasswordType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Veuillez renseigner votre mot de passe.',
+                ]),
+                new NotBlank([
+                    'message' => 'Le champ mot de passe ne doit pas être vide.',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'max' => 50,
+                    'minMessage' => 'Le champ mot de passe doit contenir au moins {{ limit }} caractéres.',
+                    'maxMessage' => 'Le champ mot de passe doit contenir au maximum {{ limit }} caractéres.',
+                ]),
+                new Regex([
+                    'pattern' => '/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s:])([^\s]){6,16}$/',
+                    'message' => 'Le champ mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractére spécial (par exemple : #, !, %, &).',
+                ]),
+            ]
+        ])
+    ;
+            }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
